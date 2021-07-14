e726e97b3ab54e25718f29bc4382eafc443de36b
Sasha Karpovich <roobel2005@gmail.com>
2021-07-15 01:39:02 +0300

    bug fixes

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -3,6 +3,10 @@ import { Currencies } from "../constants/currencies";
 export const formatCurrency = (currencyCode, cost) => {
   const currency = Currencies[currencyCode];
 
+  if (!currency) {
+    return cost;
+  }
+
   return currency.currencySymbolAtStart
     ? currency.symbol + cost
     : cost + currency.symbol;
